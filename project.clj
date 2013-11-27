539215a15ce20fa60929efe0dfb616a9b6ad1f08
Ulises Cervino Beresi <ulises@cloudant.com>
2013-11-27 23:05:09 +0000

    Tidy up project definition. Add deployment repositories.

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
 (defproject palm "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A thin wrapper around codahale's metrics."
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.codahale.metrics/metrics-core "3.0.1"]]
 
   :repositories {"repo.codahale.com" "http://repo.codahale.com"}
 
-  :global-vars {*warn-on-reflection* true})
+  :global-vars {*warn-on-reflection* true}
+
+  :deploy-branches ["master"]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]
+                 ["snapshots" {:url "https://clojars.org/repo"
+                               :creds :gpg}]])
