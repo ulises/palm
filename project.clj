2cd87ebe8615f6a6dec5aabee062e98e6144b8eb
Ulises Cervino Beresi <ulises@cloudant.com>
2014-01-30 11:53:05 +0000

    Add meters.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject palm "0.1.3-SNAPSHOT"
+(defproject palm "0.1.4-SNAPSHOT"
   :description "A thin wrapper around codahale's metrics."
 
   :dependencies [[org.clojure/clojure "1.5.1"]
---
+++ src/palm/meter.clj
@@ -0,0 +1,20 @@
+(ns palm.meter
+  (:import (com.codahale.metrics Meter)))
+
+(defn meter []
+  (Meter.))
+
+;; updating the meter
+(defn mark
+  ([^Meter meter] (do (.mark meter) meter))
+  ([^Meter meter ^long n] (do (.mark meter n) meter)))
+
+;; total number of events
+(defn count [^Meter meter] (.getCount meter))
+
+;; rates
+(defn mean-rate [^Meter m] (.getMeanRate m))
+
+(defn get-1m-rate [^Meter meter] (.getOneMinuteRate meter))
+(defn get-5m-rate [^Meter meter] (.getFiveMinuteRate meter))
+(defn get-15m-rate [^Meter meter] (.getFifteenMinuteRate meter))
